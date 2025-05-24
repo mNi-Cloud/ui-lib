f5e473d50c4c29a3ac888355fe00fa70384d8356
MuNeNiCK <mune0127@munenick.me>
2025-05-24 10:44:47 +0900

    change: stop using basepath

--- registry/new-york/blocks/template/next.config.ts
+++ registry/new-york/blocks/template/next.config.ts
@@ -2,7 +2,6 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
-    basePath: "/service",
     assetPrefix: "/service-static",
     output: 'standalone',
 }; 
